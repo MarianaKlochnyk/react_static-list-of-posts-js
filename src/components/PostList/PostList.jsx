bbf4690fa168e56f8892d97640b406c34a92ebb9
Mariana Klochnyk <klochnykmaryana@gmail.com>
2026-06-26 15:16:37 +0300

    ready

--- src/components/PostList/PostList.jsx
+++ src/components/PostList/PostList.jsx
@@ -1,1 +1,12 @@
-export const PostList = () => <>Put the list here</>;
+import { PostInfo } from '../PostInfo';
+import { CommentList } from '../CommentList';
+
+export const PostList = ({ posts }) => (
+  <>
+    <div className="PostList">
+      {posts.map((post) => (
+        <PostInfo key={post.id} post={post} />
+      ))}
+    </div>
+  </>
+);
